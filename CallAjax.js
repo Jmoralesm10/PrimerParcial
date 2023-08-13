7a725f08e0482753bd5b7cff91f1b17481511ceb
Javier Morales <jmoralesm45@miumg.edu.gt>
2023-08-12 20:46:53 -0600

    Se suben cambios

--- CallAjax.js
+++ CallAjax.js
@@ -38,3 +38,44 @@ function GET (){
         })
 
 }
+
+GETMuni();
+function GETMuni (){
+    var id = document.getElementById('Did');
+    var Mid = document.getElementById('Mid');
+    var url = "https://censopoblacion.azurewebsites.net/API/indicadores/"+id.value+"/"+Mid.value+"";
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            var data2 = JSON.parse(data);
+            
+            var valorCodigo = data2.depto_id;
+            var codigo = document.getElementById('Mcodigo');
+            codigo.textContent = valorCodigo;
+            console.log(valorCodigo);
+
+            var valorid = data2.municipio_id;
+            var id = document.getElementById('Muniid');
+            id.textContent = valorid;
+
+            var valornombre = data2.nombre;
+            var nombre = document.getElementById('Mnombre');
+            nombre.textContent = valornombre;
+            console.log(valornombre);
+
+            var valorpoblacion = data2.pob_total;
+            var pob_total = document.getElementById('Mpob_total');
+            pob_total.textContent = valorpoblacion;
+
+            var valorhombres = data2.total_sexo_hombre;
+            var hombres = document.getElementById('Mtot_hombres');
+            hombres.textContent = valorhombres;
+
+            var valormujeres = data2.total_sexo_mujeres;
+            var mujeres = document.getElementById('Mtot_mujeres')
+            mujeres.textContent = valormujeres;
+
+        })
+
+}
